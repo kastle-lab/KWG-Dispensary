afbaed82ac5c73ee1b1bf7bc4aeface943ee3fd8
Michael McCain <michaelmc1989@gmail.com>
2025-06-18 17:09:10 -0400

    fixed params and added checks for requests.

--- Deliverables/Scripts/pharmacy-extraction.py
+++ Deliverables/Scripts/pharmacy-extraction.py
@@ -4,21 +4,35 @@
 from dotenv import find_dotenv, load_dotenv
 import os
 import requests
+import json
+import pandas as pd
+import time
 
 # Functions
 
 # Variables
-statePlaceId = "ChIJwY5NtXrpNogRFtmfnDlkzeU"
-types = ["pharmacy", "drugstore"]
-apiUrl = "https://areainsights.googleapis.com/v1:computeInsights"
-
-### MAIN ### 
 env_path = find_dotenv() # Path to .env file (in root)
 load_dotenv(env_path) # Load env variables
 GOOGLE_API_KEY = os.environ['GEO_API_KEY'] # API_KEY
+query = "Pharmacies in Ohio"
+url = "https://places.googleapis.com/v1/places:searchText"
+
 params = {
-    "key": GOOGLE_API_KEY
+  "key": GOOGLE_API_KEY,
+  "textQuery": query,
+  "fields": "places.displayName,places.formattedAddress,places.businessStatus,places.location,places.id,nextPageToken",
+  "includedType":"pharmacy"
 }
-r = requests.post(apiUrl, data={'key': 'value'}, params=params)
-data = r.json()
-print(data)
+
+### MAIN ### 
+r = requests.post(url, params=params)
+
+if r.status_code == 200:
+  data = r.json()
+else:
+  print(f"Failed to retrieve data {r.status_code}\n{r.text}")
+  
+nextPageToken = data["nextPageToken"]
+
+print(json.dumps(data, indent=2))
+
